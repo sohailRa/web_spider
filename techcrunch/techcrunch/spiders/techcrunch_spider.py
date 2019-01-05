603423ce4becd9d6944946df4f1578459945777e
Sohail Rashid <sohail.ra5@gmail.com>
2019-01-04 22:49:38 -0500

    spider url

--- techcrunch/techcrunch/spiders/techcrunch_spider.py
+++ techcrunch/techcrunch/spiders/techcrunch_spider.py
@@ -3,9 +3,19 @@ import scrapy
 
 
 class TechcrunchSpiderSpider(scrapy.Spider):
+    #name of the spider
     name = 'techcrunch_spider'
+
+    #list of allowed domains
     allowed_domains = ['techcrunch.com/feed/']
-    start_urls = ['http://techcrunch.com/feed//']
+
+    #starting url for scraping
+    start_urls = ['http://techcrunch.com/feed/']
+
+    #setting the location of the output csv file
+    custom_settings = {
+        'FEED_URI' : 'tmp/techcrunch.csv'
+    }
 
     def parse(self, response):
         pass
